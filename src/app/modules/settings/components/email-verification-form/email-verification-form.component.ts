85dc260aba27055f09295eb42cf35e496a0ed22e
Stuart George <stuart@accentdesign.co.uk>
2023-05-22 21:01:27 +0100

    add error handler to verify email

--- src/app/modules/settings/components/email-verification-form/email-verification-form.component.ts
+++ src/app/modules/settings/components/email-verification-form/email-verification-form.component.ts
@@ -1,16 +1,25 @@
 import { CommonModule } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthRepository } from '@modules/auth/shared/auth.repository';
 import { AuthService } from '@modules/auth/shared/auth.service';
+import { ErrorMessagesComponent } from '@modules/shared/components/error-messages/error-messages.component';
 import { FormErrorsComponent } from '@modules/shared/components/form-errors/form-errors.component';
 import { MessageOkComponent } from '@modules/shared/components/message-ok/message-ok.component';
-import { firstValueFrom } from 'rxjs';
+import { ErrorMessageService } from '@modules/shared/services/error-message.service';
+import { finalize, first } from 'rxjs';
 
 @Component({
   selector: 'app-email-verification-form',
   standalone: true,
-  imports: [CommonModule, MessageOkComponent, ReactiveFormsModule, FormErrorsComponent],
+  imports: [
+    CommonModule,
+    MessageOkComponent,
+    ReactiveFormsModule,
+    FormErrorsComponent,
+    ErrorMessagesComponent
+  ],
   templateUrl: './email-verification-form.component.html'
 })
 export class EmailVerificationFormComponent {
@@ -25,31 +34,53 @@ export class EmailVerificationFormComponent {
 
   constructor(
     public authRepository: AuthRepository,
-    private authService: AuthService
+    private authService: AuthService,
+    private errorService: ErrorMessageService,
   ) {
   }
 
-  async requestToken(): Promise<void> {
+  request(event: Event | MouseEvent): void {
+    event.stopPropagation();
     this.requested = false;
-    try {
-      await firstValueFrom(this.authService.verifyRequest());
-      this.requested = true;
-    } catch (error) {
+    this.authService.verifyRequest().pipe(
+      first()
+    ).subscribe({
+      next: () => this.handleRequestSuccess(),
+      error: (error) => this.handleBothError(error)
+    });
+  }
+
+  submit(): void {
+    if (!this.form.valid) return;
+    this.loading = true;
+    this.success = false;
+    this.authService.verify(this.form.value.token ?? '').pipe(
+      first(),
+      finalize(() => this.handleSubmitFinish())
+    ).subscribe({
+      next: () => this.handleSubmitSuccess(),
+      error: (error) => this.handleBothError(error)
+    });
+  }
+
+  handleRequestSuccess(): void {
+    this.requested = true;
+  }
+
+  handleSubmitSuccess(): void {
+    this.success = true;
+  }
+
+  handleBothError(error: Error | HttpErrorResponse): void {
+    if (error instanceof HttpErrorResponse) {
+      this.errorService.publishError('verifyEmail', error);
+    } else {
+      this.errorService.publishMessages('verifyEmail', ['Something went wrong please try again.']);
       console.error(error);
     }
   }
 
-  async submitToken(): Promise<void> {
-    if (!this.form.valid) return;
-    this.loading = true;
-    this.success = false;
-    try {
-      await firstValueFrom(this.authService.verify(this.form.value.token ?? ''));
-      this.success = true;
-    } catch (error) {
-      console.error(error)
-    } finally {
-      this.loading = false;
-    }
+  handleSubmitFinish(): void {
+    this.loading = false;
   }
 }
