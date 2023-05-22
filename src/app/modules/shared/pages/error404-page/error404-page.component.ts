8fc70ea4daeab0c02fcd3b8aef06ceca05d8f81e
Stuart George <stuart@accentdesign.co.uk>
2023-05-22 17:00:50 +0100

    404 page

--- src/app/modules/shared/pages/error404-page/error404-page.component.ts
+++ src/app/modules/shared/pages/error404-page/error404-page.component.ts
@@ -1,10 +1,13 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-error404-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NgOptimizedImage
+  ],
   templateUrl: './error404-page.component.html'
 })
 export class Error404PageComponent {
