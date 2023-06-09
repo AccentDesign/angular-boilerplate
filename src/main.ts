3c23f590208958dde3b38e8841fcf2583037dd27
Stuart George <stuart@accentdesign.co.uk>
2023-06-09 21:09:53 +0100

    remove sentry trace

--- src/main.ts
+++ src/main.ts
@@ -17,32 +17,7 @@ if (environment.production) {
 }
 
 if (environment.sentryDsn) {
-  Sentry.init({
-    dsn: environment.sentryDsn,
-    integrations: [
-      // Registers and configures the Tracing integration,
-      // which automatically instruments your application to monitor its
-      // performance, including custom Angular routing instrumentation
-      new Sentry.BrowserTracing({
-        // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: ['localhost', /^https:\/\/example\.io\/api/],
-        routingInstrumentation: Sentry.routingInstrumentation,
-      }),
-      // Registers the Replay integration,
-      // which automatically captures Session Replays
-      new Sentry.Replay(),
-    ],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-
-    // Capture Replay for 10% of all sessions,
-    // plus for 100% of sessions with an error
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-  });
+  Sentry.init({ dsn: environment.sentryDsn });
 }
 
 const routes = [
